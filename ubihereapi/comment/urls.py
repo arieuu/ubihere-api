b34d400d859372f25f24ed3665ae69c723d61883
Pliskin <arieljanickcarvalho@gmail.com>
2024-09-04 01:26:02 +0100

    Finished CRUD for comment model

--- ubihereapi/comment/urls.py
+++ ubihereapi/comment/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.CommentListCreateAPIView.as_view())
+    path("", views.CommentListCreateAPIView.as_view()),
+    path("<int:pk>", views.CommentRetrieveUpdateDestroyAPIView.as_view())
 ]
--- ubihereapi/comment/views.py
+++ ubihereapi/comment/views.py
@@ -3,7 +3,14 @@ from rest_framework import generics
 from .serializers import CommentSerializer
 from .models import Comment
 
+# List and create
 
 class CommentListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+# Retrieve update and delete
+
+class CommentRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
